39d1e54a4b824804c59511b175e584d7bd48b767
Juan Ignacio Sánchez <jiss2891@gmail.com>
2021-01-20 12:13:10 -0300

    add screen boundaries

--- walker.py
+++ walker.py
@@ -32,7 +32,7 @@ class Walker(Sprite):
 
         self.center_position = Vector2(self.rect.center)
         self.velocity = Vector2(0, 0)
-        self.acceleration = Vector2(0, 0.1)
+        self.acceleration = Vector2(0, 0)
 
         # Sound
         self.knock = pygame.mixer.Sound("knock.mp3")
@@ -40,17 +40,32 @@ class Walker(Sprite):
     def set_skin(self):
         pass
 
+    def apply_force(self, force: Vector2):
+        self.acceleration += force
+
     def move(self):
+        self.apply_force(Vector2(0, .3))
         self.velocity += self.acceleration
         self.center_position += self.velocity
         self.rect.center = self.center_position
+        self.acceleration *= 0
 
     def bounce(self):
-        if not 0 < self.center_position.x < self.surface.get_width():
+        if not 0 < self.center_position.x:
+            self.center_position.x = 0
+            self.velocity.x *= -1
+            self.knock.play()
+        elif not self.center_position.x < self.surface.get_width():
+            self.center_position.x = self.surface.get_width()
             self.velocity.x *= -1
             self.knock.play()
 
-        if not 0 < self.center_position.y < self.surface.get_height():
+        if not 0 < self.center_position.y:
+            self.center_position.y = 0
+            self.velocity.y *= -1
+            self.knock.play()
+        elif not self.center_position.y < self.surface.get_height():
+            self.center_position.y = self.surface.get_height()
             self.velocity.y *= -1
             self.knock.play()
 
@@ -101,15 +116,19 @@ class Player(Walker):
             self.facing = FACING_WEST
             self.image = pygame.transform.flip(self.image, True, False)
 
+    def on_key_released(self, key):
+        if key in [pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN]:
+            self.acceleration = Vector2(0, 0)
+
     def on_key_pressed(self, key):
         if key == pygame.K_RIGHT:
-            self.velocity = self.velocity + Vector2(.5, 0)
+            self.apply_force(Vector2(1, 0))
         elif key == pygame.K_LEFT:
-            self.velocity = self.velocity + Vector2(-.5, 0)
+            self.apply_force(Vector2(-1, 0))
         elif key == pygame.K_UP:
-            self.velocity = self.velocity + Vector2(0, -.5)
+            self.apply_force(Vector2(0, -1))
         elif key == pygame.K_DOWN:
-            self.velocity = self.velocity + Vector2(0, .5)
+            self.apply_force(Vector2(0, 1))
         elif key == pygame.K_s:
             self.next_skin()
 
@@ -133,7 +152,7 @@ def main():
     main_clock = pygame.time.Clock()
     run = True
     display_size = Size(width=800, height=600)
-    screen = pygame.display.set_mode(display_size, pygame.FULLSCREEN, vsync=1)
+    screen = pygame.display.set_mode(display_size, pygame.RESIZABLE, vsync=1)
     sprites_image = pygame.image.load("sprites.png").convert()
     player = Player(screen, sprites_image)
     player.velocity = Vector2(3, 7)
@@ -152,6 +171,8 @@ def main():
                 run = False
             elif event.type == pygame.KEYDOWN:
                 player.on_key_pressed(event.key)
+            elif event.type == pygame.KEYUP:
+                player.on_key_released(event.key)
 
         screen.blit(background, (0, 0, display_size.width, display_size.height))
         fps_dirty = display_fps(main_clock, screen)
