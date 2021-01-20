21905ad86bb7e68a0a4c7b4af12ac98ace9ff6b3
Juan Ignacio Sánchez <jiss2891@gmail.com>
2021-01-20 06:38:16 -0300

    add Enemy and Player class

--- walker.py
+++ walker.py
@@ -1,4 +1,5 @@
 import time
+import random
 from collections import namedtuple
 
 import pygame
@@ -6,25 +7,25 @@ from pygame.sprite import Sprite
 from pygame.math import Vector2
 
 from fps.fps import display_fps
-from constants import CHARACTERS, CHARACTERS_DICT
+from constants import CHARACTERS, CHARACTERS_DICT, MOBS_DICT
 
 Size = namedtuple('Size', ['width', 'height'])
 
 FACING_EAST = 0
 FACING_WEST = 1
 
+
 class Walker(Sprite):
-    def __init__(self, surface: pygame.Surface, image: pygame.Surface):
+    def __init__(self, surface: pygame.Surface, image: pygame.Surface, skin='OLD_MAN', facing=FACING_EAST):
         super().__init__()
         self.surface = surface
 
         # Skin related stuff
-        self.skin = 'OLD_MAN'
+        self.skin = skin
         self.original_image = image
-        self.image = image.subsurface(pygame.rect.Rect(CHARACTERS_DICT.get(self.skin)))
-        self.image = pygame.transform.scale(self.image, (12 * 5, 17 * 5))
+        self.set_skin()
         self.last_skin_change = time.time()
-        self.facing = FACING_EAST
+        self.facing = facing
 
         self.rect = self.image.get_rect()
         self.rect.y = surface.get_height() - self.rect.height - 10
@@ -33,13 +34,8 @@ class Walker(Sprite):
         self.velocity = Vector2(0, 0)
         self.acceleration = Vector2(0, 0.1)
 
-    def next_skin(self):
-        if time.time() - self.last_skin_change > 1:
-            print("skin changed!")
-            self.skin = CHARACTERS[(CHARACTERS.index(self.skin) + 1) % len(CHARACTERS)]
-            self.image = self.original_image.subsurface(pygame.rect.Rect(CHARACTERS_DICT.get(self.skin)))
-            self.image = pygame.transform.scale(self.image, (12 * 5, 17 * 5))
-            self.last_skin_change = time.time()
+    def set_skin(self):
+        pass
 
     def move(self):
         self.velocity += self.acceleration
@@ -53,6 +49,45 @@ class Walker(Sprite):
         if not 0 < self.center_position.y < self.surface.get_height():
             self.velocity.y *= -1
 
+
+class Enemy(Walker):
+    def change_facing(self):
+        if self.velocity.x > 0 and not self.facing == FACING_EAST:
+            self.facing = FACING_EAST
+            self.image = pygame.transform.flip(self.image, True, False)
+        elif self.velocity.x < 0 and not self.facing == FACING_WEST:
+            self.facing = FACING_WEST
+            self.image = pygame.transform.flip(self.image, True, False)
+
+    def update(self, *args, **kwargs) -> None:
+        self.acceleration.update(
+            random.random() / 5.0,
+            -1 ** random.randrange(0, 1) * random.random() / 10.0,
+        )
+        self.move()
+        self.bounce()
+        self.change_facing()
+
+    def set_skin(self):
+        self.facing = FACING_WEST
+        skin_rect = pygame.rect.Rect(MOBS_DICT.get(self.skin))
+        self.image = self.original_image.subsurface(skin_rect)
+        self.image = pygame.transform.scale(self.image, [side * 5 for side in skin_rect.size])
+
+
+class Player(Walker):
+    def set_skin(self):
+        skin_rect = pygame.rect.Rect(CHARACTERS_DICT.get(self.skin))
+        self.image = self.original_image.subsurface(skin_rect)
+        self.image = pygame.transform.scale(self.image, [side * 5 for side in skin_rect.size])
+
+    def next_skin(self):
+        if time.time() - self.last_skin_change > 1:
+            print("skin changed!")
+            self.skin = CHARACTERS[(CHARACTERS.index(self.skin) + 1) % len(CHARACTERS)]
+            self.set_skin()
+            self.last_skin_change = time.time()
+
     def change_facing(self, key):
         if key == pygame.K_RIGHT and not self.facing == FACING_EAST:
             self.facing = FACING_EAST
@@ -60,10 +95,6 @@ class Walker(Sprite):
         elif key == pygame.K_LEFT and not self.facing == FACING_WEST:
             self.facing = FACING_WEST
             self.image = pygame.transform.flip(self.image, True, False)
-
-    def update(self, *args, **kwargs) -> None:
-        self.move()
-        self.bounce()
 
     def on_key_pressed(self, key):
         if key == pygame.K_RIGHT:
@@ -79,6 +110,10 @@ class Walker(Sprite):
 
         self.change_facing(key)
 
+    def update(self, *args, **kwargs) -> None:
+        self.move()
+        self.bounce()
+
 
 def create_background(screen: pygame.Surface, image: pygame.Surface) -> pygame.Surface:
     return pygame.transform.scale(
@@ -91,12 +126,15 @@ def main():
     pygame.init()
     main_clock = pygame.time.Clock()
     run = True
-    display_size = Size(width=600, height=480)
-    screen = pygame.display.set_mode(display_size, pygame.RESIZABLE, vsync=1)
+    display_size = Size(width=800, height=600)
+    screen = pygame.display.set_mode(display_size, pygame.FULLSCREEN, vsync=1)
     sprites_image = pygame.image.load("sprites.png").convert()
-    player = Walker(screen, sprites_image)
+    player = Player(screen, sprites_image)
     player.velocity = Vector2(3, 7)
-    all_sprites = pygame.sprite.RenderUpdates(player)
+    # Enemy
+    enemy = Enemy(screen, sprites_image, skin='BLOOD_CRYING_MOB', facing=FACING_WEST)
+    enemy.velocity = Vector2(4, 8)
+    all_sprites = pygame.sprite.RenderUpdates(player, enemy)
 
     background = create_background(screen, sprites_image)
     screen.blit(background, (0, 0))
