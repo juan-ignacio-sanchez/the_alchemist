372d7cc46cbd05e39520fded1710eae72812fb02
Juan Ignacio Sánchez <jiss2891@gmail.com>
2021-03-12 22:57:58 -0300

    fix display setting

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ SFX_VOLUME = 0.3
 
 # NOTE: at some point I should be able to remove the SCALED flag, according to this
 #       github thread: https://github.com/pygame/pygame/issues/735
-DISPLAY_MODE_FULL = RESIZABLE | SCALED | DOUBLEBUF | HWACCEL | HWSURFACE
+DISPLAY_MODE_FULL = FULLSCREEN | SCALED | DOUBLEBUF | HWACCEL | HWSURFACE
 DISPLAY_MODE_WIND = RESIZABLE | SCALED
 
 AUDIO_EXTENSION = ".ogg" if os.name == "posix" else ".wav"
