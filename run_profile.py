5be511b15224aba07ba303c6b679179c91e3edbd
Juan Ignacio Sánchez <jiss2891@gmail.com>
2021-03-20 20:20:07 -0300

    limited run_profile + updated pipenv lockfile

---
+++ run_profile.py
@@ -0,0 +1,14 @@
+import cProfile
+import pstats
+from pstats import SortKey
+
+from TheAlchemist import main as real_main
+
+
+if __name__ == '__main__':
+    cProfile.run('real_main()', 'profile.txt')
+
+    p = pstats.Stats('profile.txt')
+    p.strip_dirs().sort_stats(SortKey.TIME).print_stats(10)
+
+
